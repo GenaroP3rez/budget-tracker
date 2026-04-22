0347f6aed4f41d0f6ad78b0412e34c2a66acb2d7
lilPunny <166659824+GenaroP3rez@users.noreply.github.com>
2026-04-22 12:05:44 -0700

    attempting to add instance and getters and setters

--- src/BudgetCategory.java
+++ src/BudgetCategory.java
@@ -1,3 +1,51 @@
 public class BudgetCategory {
+
+    //instance and budget limits
+    //what is my instance is gonna have ie budget "ebery instance woukd have"
+    private double budget;
+    private double expenses;
+    private String name;
+
+    //source action 
+
+    //scanner 
+
+    public double get(){
+        return rent;
+    }
+    public void setRent(Double rent){
+        this.rent = rent;
+    }
+
+    public double getGroceries(){
+        return groceries;
+    }
+    public void setGrocceries(Double groceries){
+        this.groceries = groceries;
+    }
+    public double getDiningOut(){
+        return diningOut;
+    }
+    public void setDiningOut(Double diningOut){
+        this.diningOut = diningOut;
+    }
+    public double getTransportation(){
+        return transportation;
+    }
+    public void setTransportation(Double transportation){
+        this.transportation = transportation;
+    }
+    public double getRent(){
+        return rent;
+    }
+    public void setRent(Double rent){
+        this.rent = rent;
+    }
+
+
+    //sonic oc ahh coding 
+
+
+
     
 }
